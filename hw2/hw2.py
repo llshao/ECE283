264141b93f50b1b9e478e57c150a76e5ed90b3c2
maxwellre <yitianshao@gmail.com>
2018-04-25 02:34:42 -0700

    Two layer NN with ReLU and sigmoid. Test accuracy > 80%. L2 weight regularization does not work.

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -5,11 +5,77 @@
 import numpy as np
 import numpy.matlib as matlib
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
+#-----------------------------------------------------------------------------------------------------------------------
 # Configurations
 dimen = 2 # data dimension
 
+learnRate = 0.05
+stepNum = 10
+
+# Network parameters
+numHL1 = 64 # number of neurons in hidden layer 1
+numHL2 = 128 # number of neurons in hidden layer 1
+
+l2Factor = 0.01 # significance of L2 regularization
+
+inputDim = dimen # input data dimension
+numClass = 2 # number of classes
+
 np.random.seed(0)
+#-----------------------------------------------------------------------------------------------------------------------
+# TensorFlow initialization
+X = tf.placeholder(tf.float32, [None, inputDim])
+Y = tf.placeholder(tf.float32, [None, numClass])
+
+# Store layers weight and bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([inputDim, numHL1])),
+    'h2': tf.Variable(tf.random_normal([numHL1, numHL2])),
+    'out': tf.Variable(tf.random_normal([numHL2, numClass]))
+}
+
+biases = {
+    'b1': tf.Variable(tf.random_normal([numHL1])),
+    'b2': tf.Variable(tf.random_normal([numHL2])),
+    'out': tf.Variable(tf.random_normal([numClass]))
+}
+
+# Create neural network model
+def neural_net(x):
+    layer1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer1 = tf.nn.relu(layer1)
+
+    layer2 = tf.add(tf.matmul(layer1, weights['h2']), biases['b2'])
+    layer2 = tf.nn.relu(layer2)
+
+    layerOut = tf.matmul(layer2, weights['out']) + biases['out']
+    # layerOut = tf.sigmoid(layerOut)
+
+    return layerOut
+
+# Construct model
+logits = neural_net(X)
+prediction = tf.nn.softmax(logits)
+
+# Define loss and optimizer
+# lossOp = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y))
+lossOp = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=Y))
+
+# regularizer1 = tf.nn.l2_loss(weights['h1']) # L2 regularization on weight of hidden layer 1
+# regularizer2 = tf.nn.l2_loss(weights['h2']) # L2 regularization on weight of hidden layer 2
+# lossOp = tf.reduce_mean(lossOp + l2Factor*regularizer1 + l2Factor*regularizer2)
+optimizer = tf.train.AdamOptimizer(learning_rate=learnRate)
+trainOp = optimizer.minimize(lossOp)
+
+# Evaluate model
+correctPred = tf.equal(tf.argmax(prediction,1), tf.argmax(Y,1))
+accuracy = tf.reduce_mean(tf.cast(correctPred, tf.float32))
+
+# Initialize the variables (assign their default value)
+init = tf.global_variables_initializer()
+
 #-----------------------------------------------------------------------------------------------------------------------
 # Functions
 def gaussianFromEigen(mean_v, lamb, eig_vectors, data_num):
@@ -39,7 +105,7 @@ def evalBCResult(pLabel, tLabel):
 
 #-----------------------------------------------------------------------------------------------------------------------
 # Generating data
-default_data_num = 400
+default_data_num = 800
 
 # Class 0
 theta0 = 0
@@ -84,33 +150,54 @@ xValid = np.concatenate((x0[testEnd:,:],x1[testEnd:,:]),axis=0) # data
 tValid = np.concatenate((np.zeros(default_data_num-testEnd),np.ones(default_data_num-testEnd))) # label
 
 # Shuffle the training set
-randInd = np.random.permutation(xTrain.shape[0])
+trainLen = xTrain.shape[0]
+randInd = np.random.permutation(trainLen)
 xTrain = xTrain[randInd,:]
 tTrain = tTrain[randInd]
 
-# # Visualize data
-# plt.figure()
-# plt.get_current_fig_manager().window.wm_geometry("1400x760+20+20")
-# plt.scatter(xTrain[(tTrain==0),0],xTrain[(tTrain==0),1],s=5,label='Train (Class 0)')
-# plt.scatter(xTrain[(tTrain==1),0],xTrain[(tTrain==1),1],s=5,c='r',label='Train (Class 1)')
-# plt.scatter(xTest[(tTest==0),0],xTest[(tTest==0),1],s=5,c='g',label='Test (Class 0)')
-# plt.scatter(xTest[(tTest==1),0],xTest[(tTest==1),1],s=5,c='c',label='Test (Class 1)')
-# plt.scatter(xValid[(tValid==0),0],xValid[(tValid==0),1],s=5,c='k',label='Valid (Class 0)')
-# plt.scatter(xValid[(tValid==1),0],xValid[(tValid==1),1],s=5,c='m',label='Valid (Class 1)')
-# plt.title('Data visulization',fontsize=12)
-# plt.xlabel('Dimension 0',fontsize=10)
-# plt.ylabel('Dimension 1',fontsize=10)
-# plt_ax = plt.gca()
-# plt_ax.set_aspect('equal', 'box')
-# plt_ax.legend(loc='upper left',bbox_to_anchor=(0.1, 1.0))
-# plt.show()
+batchSize = int(np.floor(trainLen/stepNum))
+
 #-----------------------------------------------------------------------------------------------------------------------
 # 1) implement a fully connected neural network
 # (a) 1 hidden layer
 
 # (b) 2 hidden layer
+with tf.Session() as sess:
+    sess.run(init)
 
+    for step in range(stepNum):
+        currInd = range(step * batchSize, (step+1) * batchSize)
+        batch_x = xTrain[currInd, :]
+        batch_y = tTrain[currInd]
+        batch_y = np.column_stack((1-batch_y,batch_y)) # one-hot encoding
+        # Run optimization op (backprop)
+        sess.run(trainOp, feed_dict={X: batch_x, Y: batch_y})
 
+        # Calculate batch loss and accuracy
+        loss, acc = sess.run([lossOp, accuracy], feed_dict={X: batch_x, Y: batch_y})
+
+        print("Step {0:3d}: Minibatch loss = {1:5.4f} , Training accuracy = {2:4.3f}".format(step,loss,acc))
+
+    print("Optimization finished!")
+
+    # Calculate accuracy for test data
+    testY = np.column_stack((1 - tTest, tTest))  # one-hot encoding
+    print("Test accuracy = {:4.3f}".format(sess.run(accuracy, feed_dict={X: xTest, Y: testY})))
 
 #-----------------------------------------------------------------------------------------------------------------------
-print('End')
+# Visualize data
+plt.figure()
+plt.get_current_fig_manager().window.wm_geometry("1400x760+20+20")
+plt.scatter(xTrain[(tTrain==0),0],xTrain[(tTrain==0),1],s=5,label='Train (Class 0)')
+plt.scatter(xTrain[(tTrain==1),0],xTrain[(tTrain==1),1],s=5,c='r',label='Train (Class 1)')
+plt.scatter(xTest[(tTest==0),0],xTest[(tTest==0),1],s=5,c='g',label='Test (Class 0)')
+plt.scatter(xTest[(tTest==1),0],xTest[(tTest==1),1],s=5,c='c',label='Test (Class 1)')
+plt.scatter(xValid[(tValid==0),0],xValid[(tValid==0),1],s=5,c='k',label='Valid (Class 0)')
+plt.scatter(xValid[(tValid==1),0],xValid[(tValid==1),1],s=5,c='m',label='Valid (Class 1)')
+plt.title('Data visulization',fontsize=12)
+plt.xlabel('Dimension 0',fontsize=10)
+plt.ylabel('Dimension 1',fontsize=10)
+plt_ax = plt.gca()
+plt_ax.set_aspect('equal', 'box')
+plt_ax.legend(loc='upper left',bbox_to_anchor=(0.1, 1.0))
+plt.show()
